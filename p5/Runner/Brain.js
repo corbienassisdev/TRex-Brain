3caeb15448182447ba20a525b07bf55550191abb
corbienassisdev <corentin.bienassis@gmail.com>
2017-03-16 08:41:22 +0100

    [input] récupération distance entre tRex et cactus le plus proche

--- p5/Runner/Brain.js
+++ p5/Runner/Brain.js
@@ -4,7 +4,7 @@ function Brain(game) {
 	this.game = game; //reference to the game (for inputs)
 	
 	//console.log(this.game.speed); //input : OK
-	
+
 	//console.log(game.obstacles.first.x);
 	//input : distance from cactus
 	//input : largeur du cactus
@@ -17,5 +17,13 @@ function Brain(game) {
 }
 
 Brain.prototype.update = function() {
-	console.log(this.game.speed);
+	//vitesse : OK
+	//console.log(this.game.speed);
+	//distance cactus - dinosaure
+	var distance;
+	for(var i = 0; i<this.game.obstacles.length; i++) {
+		if (this.game.obstacles[i].x > this.game.tRex.x)
+			distance = console.log(this.game.obstacles[i].x - this.game.tRex.x);
+	}
+	//distance : OK (pas sûr)
 };
